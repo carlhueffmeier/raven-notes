abb3eab75199107b8afcf22031f31ce0b56d3482
saerose <sbarcelocompte@gmail.com>
2018-10-08 15:28:19 +0200

    chore: changed css little bit

--- client-desktop/src/app/shared/typography.js
+++ client-desktop/src/app/shared/typography.js
@@ -1,31 +1,39 @@
+// For now we will only use this for the NoteList component.
+
 const h1 = {
-  fontSize: '1.8em',
-  fontWeight: 700,
+  fontSize: '1.2em',
+  fontWeight: 500,
+  paddingTop: 5,
 }
 
 const h2 = {
   fontSize: '1.6em',
   fontWeight: 500,
+  padding: 5
 }
 
 const h3 = {
   fontSize: '1.4em',
   fontWeight: 400,
+  padding: 5
 }
 
 const h4 = {
   fontSize: '1.2em',
   fontWeight: 400,
+  padding: 5
 }
 
 const h5 = {
-  fontSize: '.8',
+  fontSize: '.7em',
   fontWeight: 300,
+  padding: 5
 }
 
 const p = {
-  fontSize: '1em',
+  fontSize: '.8em',
   fontWeight: 400,
+  padding: 5
 }
 export {
   h1,
