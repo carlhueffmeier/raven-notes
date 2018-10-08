e1f77a94ac12a4dd6fdcbcc128b534e44065c760
saerose <sbarcelocompte@gmail.com>
2018-10-08 14:01:44 +0200

    feat: added montserrat typo and created basic typo styles to use all over the app

---
+++ client-desktop/src/app/shared/typography.js
@@ -0,0 +1,37 @@
+const h1 = {
+  fontSize: '1.8em',
+  fontWeight: 700,
+}
+
+const h2 = {
+  fontSize: '1.6em',
+  fontWeight: 500,
+}
+
+const h3 = {
+  fontSize: '1.4em',
+  fontWeight: 400,
+}
+
+const h4 = {
+  fontSize: '1.2em',
+  fontWeight: 400,
+}
+
+const h5 = {
+  fontSize: '.8',
+  fontWeight: 300,
+}
+
+const p = {
+  fontSize: '1em',
+  fontWeight: 400,
+}
+export {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p
+}
