f43b4f7e94ca18ff9c5ff1b271085deba13622d9
saerose <sbarcelocompte@gmail.com>
2018-10-11 13:32:16 +0200

    chore: minor changes

--- client-desktop/src/app/components/NoteList/index.js
+++ client-desktop/src/app/components/NoteList/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {array} from 'prop-types';
 import SingleNote from './single';
 import { NoteListContainer } from './styles';
+import { Query } from "react-apollo";
+import { GET_NOTES_FROM_SERVER } from '../../graphql/queries';
 
 
 class NoteList extends Component {
@@ -9,13 +11,22 @@ class NoteList extends Component {
     notes: array
   }
 
+  // If no note is open on the right side, show the list width 100%, if not show the list 25vw is now.
   render() {
-    console.log('note list notes', this.props.notes)
     return (
-      <NoteListContainer>
-        {this.props.notes.map(note => <SingleNote key={note.id} note={note} /> ) || <p>Ooops...</p>}
-      </NoteListContainer>
-    )
+      <Query query={GET_NOTES_FROM_SERVER}>
+        {({loading, error, data}) => {
+          const notes = data.notes
+          if (loading) return <p>Loading...</p>
+          if (error) return <p>There's an error</p>
+          return (
+            <NoteListContainer>
+              {notes.map(note => <SingleNote key={note.id} note={note} />)}
+            </NoteListContainer>
+          )
+        }}
+      </Query>
+    );
   }
 }
 
