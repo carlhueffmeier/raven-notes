8057f69899fbe723e274d9b5cbc754d53981b0d3
saerose <sbarcelocompte@gmail.com>
2018-10-11 15:03:30 +0200

    chore: dumb sidebar is dumb

--- client-desktop/src/app/components/Sidebar/index.js
+++ client-desktop/src/app/components/Sidebar/index.js
@@ -1,69 +1,29 @@
 import React, { Component } from 'react';
 import { SidebarContainer, H3, SName, USection, GSection, Sect } from './styles';
 
-import { Query } from 'react-apollo';
-import { GET_GROUP_NOTES, GET_USER_NOTES } from '../../graphql/queries';
-
-
 class Sidebar extends Component {
-
-  state = {}
 
   render() {
     return (
       <SidebarContainer>
-        <H3><span role='img' aria-label='monkey'>🐒</span>Nikko89</H3>
+        <H3><span role='img' aria-label='monkey'>🌚</span>Nikko89</H3>
 
         {/* Getting the notes and extracts from user. */}
         <USection>
           <SName>PRIVATE</SName>
-          <Query query={GET_USER_NOTES}>
-            {({loading, error, data}) => {
-              if (loading) return <p>Loading</p>
-              if (error) return <p>There's an error...</p>
-              const notes = data.userses[0].authoredNotes
-              // This shows the second column on render.
-              if (!this.state.calledOnce) {
-                this.props.display(notes)
-                this.setState({calledOnce: true})
-              }
-              return(
-                <Sect
-                  onClick={() => this.props.display(notes)}>
-                  <span role='img' aria-label='monkey'>🌼</span>
-                  All Notes
-                </Sect>
-              )
-            }}
-          </Query>
+            <Sect>
+              <span role='img' aria-label='monkey'>🌼</span>
+              All Notes
+            </Sect>
           <Sect><span role='img' aria-label='monkey'>🌼</span> Extracts</Sect>
         </USection>
 
-
-        {/* Getting the notes from the selected group. */}
         <GSection>
-        <SName>WORKSPACE</SName>
-          <Query query={GET_GROUP_NOTES}>
-          {({loading, error, data}) => {
-            if (loading) return <p>Loading...</p>
-            if (error) return <p>There's an error.</p>
-            const groups = data.groups
-            console.log('data from query', data)
-            return (
-              groups.map((group, i) => {
-              return (
-                <Sect
-                  key={i}
-                  group={group}
-                  onClick={() => console.log(group) || this.props.display(group.notes)}>
-                    <span role='img' aria-label='monkey'>🌞</span>{groups[i].name}
-                </Sect>
-              )
-              })
-            )
-          }}
-        </Query>
-        </GSection>
+          <SName>WORKSPACE</SName>
+            <Sect>
+                <span role='img' aria-label='monkey'>🌞</span> Codeworks
+            </Sect>
+          </GSection>
       </SidebarContainer>
     );
   }
