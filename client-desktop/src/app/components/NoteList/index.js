22867e13be5b3f655d0aebe64f7b86b02cf06928
saerose <sbarcelocompte@gmail.com>
2018-10-11 09:52:48 +0200

    chore: cleaned the code a bit and added click function to my notes. User notes are hardcoded now.

--- client-desktop/src/app/components/NoteList/index.js
+++ client-desktop/src/app/components/NoteList/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import {array} from 'prop-types';
 import SingleNote from './single';
 import { NoteListContainer } from './styles';
-import { Query } from "react-apollo";
-import { GET_NOTES_FROM_SERVER, GET_CURRENT_GROUP_NOTES } from '../../graphql/queries';
 
 
 class NoteList extends Component {
@@ -11,25 +9,11 @@ class NoteList extends Component {
     notes: array
   }
 
-  // If no note is open on the right side, show the list width 100%, if not show the list 25vw is now.
   render() {
     return (
-      // <Query query={GET_CURRENT_GROUP_NOTES}>
-        // {({loading, error, data}) => {
-        // if (loading) return <p>Loading...</p>
-        // if (error) return <p>There's an error.</p>
-        // console.log('NOTE LIST', data)
-        // if (!Object.keys(data).length) data = {groups: [{notes: []}]}
-        // const notes = this.props.
-        // const notes = data
-        // console.log('notes', notes)
-        // return typeof data.groups === 'object' && !Array.isArray(data.groups) ? (
-          <NoteListContainer>
-            {this.props.notes.map(note => <SingleNote key={note.id} note={note} /> ) || <p>Ooops...</p>}
-          </NoteListContainer>
-        // ) : ''
-        // }}
-      // </Query>
+      <NoteListContainer>
+        {this.props.notes.map(note => <SingleNote key={note.id} note={note} /> ) || <p>Ooops...</p>}
+      </NoteListContainer>
     )
   }
 }
--- client-desktop/src/app/components/Sidebar/index.js
+++ client-desktop/src/app/components/Sidebar/index.js
@@ -3,60 +3,60 @@ import PropTypes from 'prop-types';
 import { SidebarContainer, H3, SName, USection, GSection, Sect } from './styles';
 
 import { Query } from 'react-apollo';
-import { GET_NOTES_FROM_SERVER, GET_GROUP_NOTES, GET_CURRENT_GROUP_NOTES } from '../../graphql/queries';
+import { GET_GROUP_NOTES, GET_USER_NOTES } from '../../graphql/queries';
 
 
 class Sidebar extends Component {
   render() {
     return (
       <SidebarContainer>
-        <H3><span role='img' aria-label='monkey'>🐒</span> User Name</H3>
+        <H3><span role='img' aria-label='monkey'>🐒</span>UserName</H3>
+
+        {/* Getting the notes and extracts from user. */}
         <USection>
           <SName>PRIVATE</SName>
-
-          <Query query={GET_NOTES_FROM_SERVER}>
-            {({loading, error, data, client}) => {
+          <Query query={GET_USER_NOTES}>
+            {({loading, error, data}) => {
+              if (loading) return <p>Loading</p>
+              if (error) return <p>There's an error...</p>
+              const notes = data.userses[0].authoredNotes
+              console.log('users notes', data.userses)
               return(
-                <Sect onClick={() => client.writeQuery({ query: GET_GROUP_NOTES, data})}>
+                <Sect
+                  onClick={() => this.props.display(notes)}>
                   <span role='img' aria-label='monkey'>🌼</span>
                   All Notes
                 </Sect>
               )
             }}
           </Query>
-
           <Sect><span role='img' aria-label='monkey'>🌼</span> Extracts</Sect>
         </USection>
+
+
+        {/* Getting the notes from the selected group. */}
         <GSection>
         <SName>WORKSPACE</SName>
-
-        {/* Testing time */}
-        <Query query={GET_GROUP_NOTES}>
-        {({loading, error, data, client}) => {
-          if (loading) return <p>Loading...</p>
-          if (error) return <p>There's an error.</p>
-          const groups = data.groups
-          console.log('data from query', data)
-          return (
-            groups.map((group, i) => {
+          <Query query={GET_GROUP_NOTES}>
+          {({loading, error, data}) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>There's an error.</p>
+            const groups = data.groups
+            console.log('data from query', data)
             return (
-              <Sect
-                key={i}
-                group={group}
-                onClick={() => console.log(group) || this.props.display(group.notes)}
-                // onClick={() => {client.writeQuery({ query: GET_CURRENT_GROUP_NOTES, data: { groups: groups[i] }})}}
-              >
-                <span role='img' aria-label='monkey'>🌞</span>{groups[i].name}
-              </Sect>
+              groups.map((group, i) => {
+              return (
+                <Sect
+                  key={i}
+                  group={group}
+                  onClick={() => console.log(group) || this.props.display(group.notes)}>
+                    <span role='img' aria-label='monkey'>🌞</span>{groups[i].name}
+                </Sect>
+              )
+              })
             )
-          })
-          // Map sections and return notes from indexOf section
-          )
-        }}
+          }}
         </Query>
-        {/* No more testing */}
-        <Sect><span role='img' aria-label='monkey'>🌞</span>Sassy Designers</Sect>
-        <Sect><span role='img' aria-label='monkey'>🌞</span>ReactJS</Sect>
         </GSection>
       </SidebarContainer>
     );
