a60e95d348c2658b42936c6a5c0857ea0dd298b6
Rodrigo Sanchez <rodrifs@gmail.com>
2018-10-13 12:12:39 +0200

    refactor: deleted unused actions and types

--- client-desktop/src/app/redux/modules/currentNote/types.js
+++ client-desktop/src/app/redux/modules/currentNote/types.js
@@ -1,5 +1,4 @@
 const SELECT_CURRENT_NOTE = 'app/ui/currentNote/select';
 const RESET_CURRENT_NOTE = 'app/ui/currentNote/reset';
-const UPDATE_CURRENT_NOTE = 'app/ui/currentNote/update'
 
-export { SELECT_CURRENT_NOTE, RESET_CURRENT_NOTE, UPDATE_CURRENT_NOTE };
+export { SELECT_CURRENT_NOTE, RESET_CURRENT_NOTE };
