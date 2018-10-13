c1661a18abd8599d5b924535f534cc2db44ffcff
saerose <sbarcelocompte@gmail.com>
2018-10-13 15:33:12 +0200

    chore: deleted non used styling code

--- client-desktop/src/app/styles.js
+++ client-desktop/src/app/styles.js
@@ -20,34 +20,3 @@ injectGlobal`
     box-sizing: border-box;
   }
 `;
-
-// this is not working
-const Buttons = styled('div')`
-  background-color: red;
-  width: 10vw;
-  margin: 0;
-`;
-const P1 = styled('button')`
-  position: absolute;
-  margin-top: 50px;
-  left: 90vw;
-  height: 10vh;
-  background-color: lavender;
-`;
-
-const P2 = styled('button')`
-  position: absolute;
-  margin-top: 120px;
-  left: 90vw;
-  height: 10vh;
-  background-color: pink;
-`;
-const P3 = styled('button')`
-  position: absolute;
-  margin-top: 200px;
-  left: 90vw;
-  height: 10vh;
-  background-color: peachpuff;
-`;
-
-export { P1, P2, P3, Buttons };
