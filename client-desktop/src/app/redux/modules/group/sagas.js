b1ec218ef21e5561fd5d82d1e497adb7403d953d
saerose <sbarcelocompte@gmail.com>
2018-10-15 20:06:00 +0200

    fix: groupSchema is not an array... oops

--- client-desktop/src/app/redux/modules/group/sagas.js
+++ client-desktop/src/app/redux/modules/group/sagas.js
@@ -3,19 +3,19 @@ import { normalize } from 'normalizr';
 import graphqlClient from '../../../api/graphqlClient';
 import * as actions from './actions';
 import { CREATE_GROUP } from './types';
-import { CREATE_GROUP_MUTATION } from './graphql';
-// import { ALL_NOTES_QUERY, CREATE_NOTE_MUTATION, UPDATE_NOTE_MUTATION } from './graphqlMock';
+// import { CREATE_GROUP_MUTATION } from './graphql';
+import { CREATE_GROUP_MUTATION } from './graphqlMock';
 import { group as groupSchema } from './schema';
 
 // const POLL_INTERVAL = 1000 * 10; // 10s
 
 function* createGroup(action) {
   try {
-    console.log('in graphql', action)
     const { payload: name } = action
     const response = yield call([graphqlClient, 'request'], CREATE_GROUP_MUTATION, { name });
     // ⚠️ Caution: Necessary change for local server
-    const normalizedData = normalize(response.createGroup, [groupSchema]);
+    const normalizedData = normalize(response.createGroup, groupSchema);
+    console.log('data', normalizedData)
     yield put(actions.createGroupSuccess(normalizedData));
   } catch (error) {
     console.error(error);
@@ -24,7 +24,6 @@ function* createGroup(action) {
 }
 
 function* groupSaga() {
-  console.log('yo!')
   yield takeEvery(CREATE_GROUP.START, createGroup);
   // yield spawn(pollFetchGroups);
 }
