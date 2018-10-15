396f22020c1b22e03d7e9e8053c08699600f5fda
saerose <sbarcelocompte@gmail.com>
2018-10-15 14:05:51 +0200

    chore: search implemented

--- client-desktop/src/app/containers/NoteListContainer.js
+++ client-desktop/src/app/containers/NoteListContainer.js
@@ -2,41 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectors as noteSelectors } from '../redux/modules/note';
 import { actions as currentNoteActions } from '../redux/modules/currentNote';
+
+import { actions as searchActions, selectors as searchSelectors } from '../redux/modules/search';
 import NoteList from '../components/NoteList';
 import { sortBy, dateDescending, prop } from '../lib/utils';
 
 class NoteListContainer extends Component {
 
-  state = {
-    searchQuery: ''
-  };
-
-  onQueryChange = newQuery => {
-    this.setState({ searchQuery: newQuery});
-  };
-
   render() {
-    const { notes, selectNote } = this.props;
-    const noteFilter = new RegExp(this.state.searchQuery, 'i');
+    const { notes, selectNote, searchQuery } = this.props;
+    const noteFilter = new RegExp(searchQuery, 'i');
     const filteredNotes = notes.filter(note => noteFilter.test(note.contentText));
 
     return (
       <NoteList notes={filteredNotes}
         selectNote={selectNote}
-        onQueryChange={this.onQueryChange} />
+        onQueryChange={this.props.onQueryChange} />
     );
   }
 }
 
 function mapStateToProps(state) {
   const groupNotes = noteSelectors.getCurrentGroupNotesWithAuthor(state);
+
   return {
-    notes: sortBy(dateDescending(prop('updatedAt')), groupNotes)
+    notes: sortBy(dateDescending(prop('updatedAt')), groupNotes),
+    searchQuery: searchSelectors.getCurrentSearch(state)
   };
 }
 
 const mapDispatchToProps = {
-  selectNote: currentNoteActions.selectNote
+  selectNote: currentNoteActions.selectNote,
+  onQueryChange: searchActions.changeQuery
 };
 
 export default connect(
