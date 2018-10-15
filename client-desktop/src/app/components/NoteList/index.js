64648170d7ae7c555479a4d59486526a4d44b403
saerose <sbarcelocompte@gmail.com>
2018-10-15 10:56:23 +0200

    feat: three column layout works when clicking a note in the two column kayout

--- client-desktop/src/app/components/NoteList/index.js
+++ client-desktop/src/app/components/NoteList/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import SingleNote from './single';
 import SearchBar from '../SearchBar/index';
 import CreateNoteContainer from '../../containers/CreateNoteContainer';
 import { NoteListContainer, SearchAndCreateContainer } from './styles';
 
+import { actions as layoutActions, constants as layoutConstants } from '../../redux/modules/layout';
+
+
 class NoteList extends Component {
   render() {
-    const { notes, selectNote } = this.props;
+    const { notes, selectNote, changeLayout } = this.props;
+
     return (
       <NoteListContainer>
         <SearchAndCreateContainer>
@@ -15,7 +21,11 @@ class NoteList extends Component {
         </SearchAndCreateContainer>
         <div>
           {notes.map(note => (
-            <SingleNote key={note.id} note={note} onClick={() => selectNote(note.id)} />
+            <SingleNote key={note.id} note={note} onClick={
+              () => {
+                selectNote(note.id)
+                changeLayout(layoutConstants.THREE_COLUMN_LAYOUT)
+              }} />
           ))}
         </div>
       </NoteListContainer>
@@ -23,4 +33,4 @@ class NoteList extends Component {
   }
 }
 
-export default NoteList;
+export default connect (null, layoutActions)(NoteList);
