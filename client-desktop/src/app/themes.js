e21d7ab6741fa518cceea9380f4b7c91b431ee7a
saerose <sbarcelocompte@gmail.com>
2018-10-17 16:45:03 +0200

    chore: changing colors from dark mode

--- client-desktop/src/app/themes.js
+++ client-desktop/src/app/themes.js
@@ -2,6 +2,7 @@ const lightTheme = {
   colors: {
     primary: '#fec66c',
     secondary: '#F7F6F3',
+    hightlight: '#3A2401',
     plight: '#FFFFFF',
     pdark: '#C4C3C0',
     text: '#000000',
@@ -20,21 +21,22 @@ const lightTheme = {
 
 const darkTheme = {
   colors: {
-    primary: '#fec66c',
-    secondary: '#F7F6F3',
-    plight: '#FFFFFF',
-    pdark: '#C4C3C0',
-    text: '#000000',
-    textHover: 'peachpuff',
-    textHardSelect: 'orange',
-    h1: '#2f4f4f',
-    h2: '#708090',
-    h3: '#778899',
-    sectionTitle: '#778899',
-    h4: '#696969',
-    h5: '#808080',
-    h6: '#a9a9a9',
-    softWarning: '#ff5722'
+    primary: '#FEC66C',
+    secondary: '#212121',
+    hightlight: '#CCCCCC',
+    plight: '#303030',
+    pdark: '#FFFFFF',
+    text: '#FFFFFF',
+    textHover: '#EOEOEO',
+    textHardSelect: '#F5F5F5',
+    h1: '#ECF0F1',
+    h2: '#F0F3F4',
+    h3: '#F4F6F7',
+    sectionTitle: '#F0F3F4',
+    h4: '#F7F9F9',
+    h5: '#FBFCFC',
+    h6: '#FDFEFE',
+    softWarning: '#FDEDEC'
   }
 };
 
